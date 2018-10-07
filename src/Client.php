e821a965fc77febc738940164d59dd712c90bfa7
kuromoka <kuromokapb@gmail.com>
2018-10-08 00:11:33 +0900

    Add getBtcPrice method

--- src/Client.php
+++ src/Client.php
@@ -10,4 +10,25 @@ class Client
     {
         echo "tech5-bitcoin\n";
     }
+
+    public function getBtcPrice() {
+        // Guzzleのインスタンスを作成
+        $client = new \GuzzleHttp\Client();
+
+        // https://api.bitflyer.com/v1/getticker?product_code=BTC_JPY のGETリクエストでTickerを呼び出す
+        $response = $client->request('GET', 'https://api.bitflyer.com/v1/getticker', [
+            'query' => ['product_code' => 'BTC_JPY']
+        ]);
+
+        // $response->getBody()->getContents()で、APIからのレスポンスを取得できる
+        // 結果はJSONで返却されるのでデコード
+        $result = json_decode($response->getBody()->getContents(), true);
+
+        // ltpキーにBitcoinに対する日本円の最終取引価格が入る
+        if ($result['ltp']) {
+            return $result['ltp'];
+        } else {
+            return null;
+        }
+    }
 }
